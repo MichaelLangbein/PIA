4ff9fb59104cc825cfd9f42cf3f6d438de7304ce
Langbein, Michael <michael.langbein@dlr.de>
2021-10-05 15:42:50 +0200

    wps setup

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+import pywps
+from pywps import Service
+
+
 
 app = Flask(__name__)
+# service = Service([myService], ['pywps.cfg'])
+wpsService = Service([])
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/wps', methods=['GET', 'POST'])
+def wps():
+    return wpsService
